cde56abbb4314e0838ff3e6768b9fa9a4b5daa6e
Jose Montes <josemontesp@gmail.com>
2019-02-12 22:01:58 -0800

    ignore dist

--- dist/routes/author-post.js
+++ dist/routes/author-post.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const models_1 = require("../models/models");
 const routes_helpers_1 = require("./routes-helpers");
-const controller = (bodyParams) => {
+exports.controller = (bodyParams) => {
     return new models_1.AuthorModel(bodyParams).save();
 };
-exports.handler = routes_helpers_1.getHandler(controller);
+exports.handler = routes_helpers_1.getAWSLambdaHandler(exports.controller);
--- dist/routes/routes-helpers.js
+++ dist/routes/routes-helpers.js
@@ -1,9 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const errors_1 = require("../errors");
+/** Initial setup for the AWS lambda function. */
 exports.setupLambda = (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
 };
+/**
+ * Parses the body of the request and throws BodyParserError if not successful.
+ */
 exports.parseBody = (body) => {
     try {
         return JSON.parse(body);
@@ -12,14 +16,20 @@ exports.parseBody = (body) => {
         throw new errors_1.BodyParserError(error);
     }
 };
-exports.handleError = (error, status = 500) => {
+/** Constructs an error response based on the provided error object. */
+exports.getErrorResponse = (error, status = 500) => {
     const statusCode = error.statusCode || status;
     return {
         body: JSON.stringify({ statusCode, message: error.message }),
         statusCode,
     };
 };
-exports.getHandler = (controller) => {
+/**
+ * Receives a controller function and returns a handler function usable as an
+ * AWS lambda function. Useful because it abstracts all the setup logic and
+ * error handling.
+ */
+exports.getAWSLambdaHandler = (controller) => {
     return async (event, context, callback) => {
         try {
             exports.setupLambda(event, context, callback);
@@ -31,7 +41,7 @@ exports.getHandler = (controller) => {
             };
         }
         catch (error) {
-            return exports.handleError(error);
+            return exports.getErrorResponse(error);
         }
     };
 };
